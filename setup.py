59714448447c7b31e3cdfda2b7f96ccf7c71b3d1
R. Biswas <rbiswas4@gmail.com>
2015-06-25 17:28:24 -0700

    modified: simlibs/summarize_opsim.py :         Modified so that there is a function to deal with the individual         fields IDs rather that a groupby object modified: example/CompareSimlibs.ipynb :         Added methods to split header info The following files were missed in git commits before new file:   setup.py : new file:   simlibs/__init__.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#from ez_setup import use_setuptools
+#use_setuptools()
+from setuptools import setup, find_packages
+
+setup(# package information
+      name="generatesimlibs",
+      version="0.0.1dev",
+      description='Simple repo setup to check documentation',
+      long_description=''' ''',
+      # What code to include as packages
+      packages=['simlibs'],
+      # What data to include as packages
+      include_package_data=True,
+      # package_data={'': ['example_data/*.dat']}
+      )

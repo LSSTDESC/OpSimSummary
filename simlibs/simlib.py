b8d2f69ad4ac6036903293d4bf573f023d5baecb
R. Biswas <rbiswas4@gmail.com>
2015-07-11 18:24:14 -0700

    Added documentation, no change to code

--- simlibs/simlib.py
+++ simlibs/simlib.py
@@ -8,23 +8,39 @@ class fieldSimLib(object):
     from cStringIO import StringIO
     '''
     Class to hold data corresponding to a particular fieldID (LIBID) of a
-    SNANA SIMLIB file and methods
-    '''
+    SNANA SIMLIB file and methods. The fieldSimlib class for a particular field
+    has the following attributes, and may be instantiated by supplying these, 
+    or can be conveniently constructed from the string corresponding to the
+    description of this data in an SNANA simlib file using the constructor
+    fromSimlibString:
 
+    Attributes
+    ----------
+    fieldID: int
+        a unique integer identifying the field of view. Different pointings
+        of the same field at different times (but possibly with dithers) are
+        associated with the same fieldID
+    meta: dict
+        metadata associated with the field, which has at least the following
+        keys:
+        LIBID, RA, DECL, MWEBV, NOBS, PIXSIZE
+    data: `~pd.DataFrame` object with the observations and having at least the
+        following columns: 'MJD', 'IDEXPT', 'FLT', 'GAIN', 'NOISE', 'SKYSIG',
+        'PSF1', 'PSF2', 'PSFRatio', 'ZPTAVG', 'ZPTERR', 'MAG']. The meanings of
+        these columns are discussed in the SNANA manual in the sub-section
+        'The 'SIMLIB' Observing file (4.7)
+    '''
     def __init__(self, simlibdata, simlib_meta):
 
         self.data = simlibdata 
         self.meta = simlib_meta
         self.fieldID = self.meta['LIBID']
 
-        # self.getFieldSimlib(simlibstring)
-        # self.validate()
-
     @classmethod
     def fromSimlibString(cls, simlibstring):
         '''
         Basic constructor method to take a string corresponding to a
-        simlib data corresponding to a single LIBID and parse it to
+        field simlib data corresponding to a single LIBID and parse it to
         metadata containing the properties of the field, a
         `~pandas.DataFrame` containing the data, and the string after the
         data
@@ -47,6 +63,18 @@ class fieldSimLib(object):
 
     def validate(self, validate_string):
         '''
+        Validate the interpretation of the field simlib data from a field simlib
+        string by checking 
+        1. the LIBID at the end of the string matches the one at the beginning
+            (ie. somehow multiple fields have not been read in)
+        2. the number of rows of the data for this field simlib matches the
+        number of observations recorded in the metadata as NOBS
+
+        Parameters
+        ----------
+        validate_string: string, mandatory
+            footer obtained by splitting the simlib corresponding to the field
+            usually of the form 
         '''
         val = eval(validate_string.split()[-1])
         if int(self.meta['LIBID']) != val:
@@ -62,7 +90,8 @@ class fieldSimLib(object):
     @staticmethod
     def split_simlibString(simlibString):
         '''
-        split the string into header, footer, and data pieces
+        split the string corresponding to a simlib file into header, footer,
+        and data pieces
         '''
         lst = simlibString.split('MAG')
         header = lst[0]
@@ -73,7 +102,7 @@ class fieldSimLib(object):
     @staticmethod
     def simlibdata(data):
         '''
-        manipulate data piece to form pandas DataFrame object
+        manipulate string in the simlibstring to form pandas DataFrame object
         '''
         fhandle = StringIO(data)
         df = pd.read_csv(fhandle, delimiter="\s+",
@@ -120,7 +149,6 @@ class simlib(object):
         self.simlibdicts = self.getSimlibs(simlibFile)
         self.fieldIDs = self.simlibdicts.keys()
     
-    # @property
     def simlibData(self, fieldID):
        return self.simlibdicts[fieldID] 
 

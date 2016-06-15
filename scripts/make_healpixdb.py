7d68509f98281a052a6dbd2a1cedb44d7106ed3e
R. Biswas <rbiswas4@gmail.com>
2016-06-14 23:55:27 -0700

    Adding script to create database: First try 	new file:   make_healpixdb.py

---
+++ scripts/make_healpixdb.py
@@ -0,0 +1,40 @@
+from __future__ import division
+import numpy as np
+import time
+import sqlite3
+import healpy as hp
+from healpy import query_disc, query_polygon
+import opsimsummary as oss
+import pandas as pd
+from itertools import repeat
+
+opsim_hdf = '/Users/rbiswas/data/LSST/OpSimData/minion_1016.hdf'
+OpSim_combined = pd.read_hdf(opsim_hdf, 'Table')
+
+def addVec(df, raCol='ditheredRA', decCol='ditheredDec'):
+    thetas  = - df[decCol] + np.pi /2.
+    phis = df[raCol]
+    df['vec'] = list(hp.ang2vec(thetas, phis))
+
+addVec(OpSim_combined, raCol=raCol, decCol=decCol)
+NSIDE = 256
+OpSim_combined['hids'] = [query_disc(NSIDE, vec, np.radians(1.75), inclusive=True) for vec in OpSim_combined.vec]
+lens = map(len, OpSim_combined.hids.values)
+
+rowdata = []
+_ = list(rowdata.extend(repeat(i, lens[i])) for i in xrange(len(OpSim_combined)))
+coldata = np.concatenate(OpSim_combined.hids.values)
+
+conn = sqlite3.Connection('healpixels.db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE simlib (obsHistID int, ipix int)')
+tstart = time.time()
+for i in range(len(rowdata)):
+    cur.execute('INSERT INTO simlib VALUES ({0}, {1})'.format(rowdata[i], coldata[i]))
+    if i % 10000000 == 0:
+        conn.commit()
+        tat = time.time()
+        print('committed at {0} taking time {1}'.format(i, tat-tstart))
+tend = time.time()
+conn.commit()
+conn.close()

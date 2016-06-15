c0a7ec20648ce760cd5d1da761a70e32dd8466a0
R. Biswas <rbiswas4@gmail.com>
2016-06-15 08:04:04 -0700

    Script used to generate sqlite database, no indices made. The output is 7.6 GB. 	modified:   scripts/make_healpixdb.py

--- scripts/make_healpixdb.py
+++ scripts/make_healpixdb.py
@@ -16,7 +16,7 @@ def addVec(df, raCol='ditheredRA', decCol='ditheredDec'):
     phis = df[raCol]
     df['vec'] = list(hp.ang2vec(thetas, phis))
 
-addVec(OpSim_combined, raCol=raCol, decCol=decCol)
+addVec(OpSim_combined)
 NSIDE = 256
 OpSim_combined['hids'] = [query_disc(NSIDE, vec, np.radians(1.75), inclusive=True) for vec in OpSim_combined.vec]
 lens = map(len, OpSim_combined.hids.values)
@@ -29,12 +29,14 @@ conn = sqlite3.Connection('healpixels.db')
 cur = conn.cursor()
 cur.execute('CREATE TABLE simlib (obsHistID int, ipix int)')
 tstart = time.time()
+told = tstart
 for i in range(len(rowdata)):
     cur.execute('INSERT INTO simlib VALUES ({0}, {1})'.format(rowdata[i], coldata[i]))
     if i % 10000000 == 0:
         conn.commit()
         tat = time.time()
-        print('committed at {0} taking time {1}'.format(i, tat-tstart))
-tend = time.time()
+        print('committed at {0} taking time {1}'.format(i, tat-told))
+        told = tat
+
 conn.commit()
 conn.close()

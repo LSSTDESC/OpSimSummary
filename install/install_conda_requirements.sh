c1e4b45ca997b3d43704f4737f9908e261401e7e
Christian <aerosetzer@gmail.com>
2018-01-16 15:26:17 +0100

    Added yes flag to the conda install script.

--- install/install_conda_requirements.sh
+++ install/install_conda_requirements.sh
@@ -1,3 +1,3 @@
-conda install --no-update-dependencies -c anaconda -v --file ./install/conda_requirements_anaconda.txt
-conda install --no-update-dependencies -c conda-forge -v --file ./install/conda_requirements_conda_forge.txt
+conda install --no-update-dependencies -c anaconda -v --yes --file ./install/conda_requirements_anaconda.txt
+conda install --no-update-dependencies -c conda-forge -v --yes --file ./install/conda_requirements_conda_forge.txt
 conda list --explicit > ./install/spec-file.txt;

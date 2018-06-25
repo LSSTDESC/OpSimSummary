8674d3997c13a65afbb1d8bc3a520fd0e07c0dfc
R. Biswas <rbiswas4@gmail.com>
2018-06-25 20:34:45 +0200

    Adding New library methods and script for creating simlibs with simlibs now using git cherry-pick splitting the OpSim Combined fields into fields that have DDF visits and fields that do not have DDF visits. If there are no dithers on the DDF as assumed, this can be done by the number of visits.
    	modified:   opsimsummary/summarize_opsim.py
    - Added Library methods to `SynOpSim`: `observedVistsinRegion` to
      essentially do most of the job of `sampleRegion` which is now not
    necesary. This returns a dataFrame with fields, and the number of
    visits.
    	modified:   opsimsummary/simlib.py
    - Added Library methods to `Simlib`. :
        - `SimlibField` which should be expanded later into the `Simlib`
          class
        - `get_surveyPix` : which gets the chosen fields
        - `simlibs_for_fields` : which actually produces the simlib fields
          for writing out.
        - `randomSimlibs` is now redundant
    	modified:   opsimsummary/version.py
        - bumped version
    	new file:   scripts/make_simlibs.py
        - added script with argparse
    
    Fixed comments in the `genericSimlibs` method
    	modified:   scripts/make_simlibs.py
    
    Adding installed conda packages in spec-files.
    	modified:   ../install/spec-file.txt
    
    Fixing booleans in script
    	modified:   make_simlibs.py
    
    Documenting method in make_simlibs script
    	modified:   make_simlibs.py

--- opsimsummary/summarize_opsim.py
+++ opsimsummary/summarize_opsim.py
@@ -179,6 +179,47 @@ class SynOpSim(object):
                 idx = x.query('dist < @prad').index
                 yield self.df_subset_columns(self.pointings.loc[idx], subset)
 
+
+    def observedVisitsinRegion(self, nside=256, nest=True, minVisits=1,
+                               maxVisits=None, outFile=None, writeFile=False):
+        """
+        return a `pd.DataFrame` with the healpixelID='hid', ra and dec of the
+        healpixels 'hpix_ra', 'hpix_dec' in radians, and count the number of
+        visits for all healpixels that have visits satisfying
+        minVisits <= count <= maxVisits . This will also write out a csv
+        outFile clobbering past version with this dataframe. 
+        """
+        if self.usePointingTree is False:
+            raise NotImplementedError('This method works only with `PointingTree`')
+
+        if writeFile:
+            assert outFile is not None
+
+        ipix = np.arange(hp.nside2npix(nside))
+        hpix_ra, hpix_dec = hp.pix2ang(nside, ipix, nest=nest, lonlat=True)
+
+        # Input array for Tree query
+        X = np.zeros(shape=(len(hpix_ra), 2))
+        X[:, 0] = np.radians(hpix_dec)
+        X[:, 1]= np.radians(hpix_ra)
+
+        # count visits for each healpixel in the sky
+        counts = self.pointingTree.tree.query_radius(X, r=np.radians(1.75),
+                                                     count_only=True)
+        survey = pd.DataFrame(dict(hid=ipix, ra=hpix_ra, dec=hpix_dec,
+                                   numVisits=counts)).set_index('hid')
+
+        # maxVisits is None, imply don't apply maxVisits 
+        if maxVisits is None:
+            maxVisits =  counts.max() + 1
+
+        survey = survey.query('numVisits >= @minVisits and numVisits <=@maxVisits')
+        if writeFile:
+            survey.to_csv(outFile)
+        return survey
+
+
+
     def sampleRegion(self, numFields=50000, minVisits=1, nest=True, nside=256,
                      rng=np.random.RandomState(1), outfile=None,
                      usePointingTree=True, subset='wfd', mwebv=0.):

7a20ec198ffabeb560d5bd27dc54b0da50926248
R. Biswas <rbiswas4@gmail.com>
2019-04-15 21:02:05 +0200

    Adding echo to git travis install script 	modified:   install/install_conda_requirements.sh

--- install/install_conda_requirements.sh
+++ install/install_conda_requirements.sh
@@ -1,3 +1,5 @@
 conda install -c anaconda -v --yes --file ./install/conda_requirements_anaconda.txt
+echo "Done install conda requirements from anaconda"
 conda install -c conda-forge -v --yes --file ./install/conda_requirements_conda_forge.txt
+echo "Done install conda requirements from conda forge"
 conda list --explicit > ./install/spec-file.txt;

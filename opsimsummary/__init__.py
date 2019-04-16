9981735b1f01419fe81fef73e0d927600a14f9a9
R. Biswas <rbiswas4@gmail.com>
2019-04-16 11:49:01 +0200

    Simplified tests, removed test_healpix.py which is not needed 	modified:   ../opsimsummary/__init__.py 	deleted:    test_healpix.py

--- opsimsummary/__init__.py
+++ opsimsummary/__init__.py
@@ -4,23 +4,18 @@ from .summarize_opsim import *
 from . import summarize_opsim
 from .simlib import *
 from .version import __VERSION__ as __version__
-here = __file__
-basedir = os.path.split(here)[0]
-example_data = os.path.join(basedir, 'example_data')
 from .trig import *
-from .healpix import *
-from .tessellations import *
+# from .tessellations import *
 from .opsim_out import *
-from .healpixTiles import *
-import os
 
 here = __file__
 basedir = os.path.split(here)[0]
 example_data = os.path.join(basedir, 'example_data')
-try:
-    from .visualization import *
-except ImportError as e:
-    print('Some imports failed, which implies some dependencies are missing as described below')
-    print(e)
-    print('Visulization functions based on maps will not work')
-    pass
+
+# from .healpix import *
+# from .healpixTiles import *
+# import os
+
+# here = __file__
+# basedir = os.path.split(here)[0]
+# example_data = os.path.join(basedir, 'example_data')
